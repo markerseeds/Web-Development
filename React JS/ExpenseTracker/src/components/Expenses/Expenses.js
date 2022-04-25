5fb8cf2db536e20b8d81dd7d4747d26eefa5f6c9
markerseeds <mark.see86@gmail.com>
2022-04-25 16:39:49 +0800

    First Upload

---
+++ React JS/ExpenseTracker/src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import ExpensesList from "./ExpensesList";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
+
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses}/>
+    </Card>
+  );
+}
+
+export default Expenses;
